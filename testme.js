ce328765dd26a08b2d47fc524af5138b7330bae6
chrisoddy1 <taiwo.odeniyi@yhoo.com>
2026-07-11 10:16:22 +0100

    new

--- testme.js
+++ testme.js
@@ -1,9 +1,48 @@
- const hellem = () => {
-    
-     console.log('i am good')
-     
-     
+// const hellem = () => {
+// 	console.log("i am good");
+// };
 
+// hellem();
+
+
+// let mixed = { name: "john", age: 20, isLogin: true };
+
+// console.log(mixed.name);
+
+// //function
+
+// const getBy = () => {
+//     const age = 35
+//     return age;
+
+// }
+// const red = getBy();
+// console.log(red);
+
+console.log(3 + 3)
+console.log(3 - 5)
+console.log(3 * 4)
+console.log(6 / 3)
+console.log(2 ** 3)
+console.log(7 % 2)
+
+
+//--------------------------------------
+
+console.log(3 === 5);
+console.log(2 !== 4);
+console.log(3 > 6);
+console.log(5 <7 )
+console.log(5 >= 7)
+console.log(5<=6)
+
+
+///Logical Operator
+
+if (3 === 2) {
+    console.log('numbers correct')
+} else { 
+    console.log('wrong')
 }
 
-helleme()
+
