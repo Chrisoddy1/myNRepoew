f88c7bca99947f977577e648952424620cb7fd25
chrisoddy1 <taiwo.odeniyi@yhoo.com>
2026-07-13 08:32:47 +0100

    latest

--- jelly.js
+++ jelly.js
@@ -1,1 +1,13 @@
 //ggggg
+const q = 3;
+const x = a,
+
+if (q === x) {
+	console.log("not correct");
+} else if (q > x) {
+	console.log("not correct 1");
+} else if (q < x) { 
+	console.log("low");
+} else {
+	console.log("invalid");
+}
