ce68dcf32144b2beaa06bc7d86afc37e3762e49a
Chrisoddy1 <TAIWO.ODENIYI@YAHOO.COM>
2026-07-14 07:25:21 +0100

    Update mytest.spec.js
    BBC login

--- tests/mytest.spec.js
+++ tests/mytest.spec.js
@@ -1,3 +1,8 @@
 import { test, expect } from '@playwright/test'
 
-test.beforeEach('ggggg')
+test('launch browser'async ({page})=>{
+    
+  await page.goto('www.bbc.co.uk');
+
+  await page.locator().click();
+})
